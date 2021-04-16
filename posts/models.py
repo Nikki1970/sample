addc71af5f8404088610c9fba4cf1a5963feb4c6
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-16 15:15:20 +0530

    Create model for post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=512)
+    slug = models.SlugField(max_length=512)
+    body = models.TextField()
+    author = models.ForeignKey('auth.User', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return "%s by %s" % (self.title, self.author.username)
