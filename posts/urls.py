1b1a0bf348ea1a7a86659d5e5a819d775e216397
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-16 16:38:29 +0530

    Add view for editing post

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_list),
-    path('<slug:slug>/', views.post_detail, name="post-detail")
+    path('<slug:slug>/', views.post_detail, name="post-detail"),
+    path('<slug:slug>/edit/', views.post_edit, name="post-edit")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 
 def posts_list(request):
@@ -19,3 +20,21 @@ def post_detail(request, slug):
         'post': post
     }
     return render(request, 'detail.html', context)
+
+
+def post_edit(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('post-detail', slug=post.slug)
+
+    context = {
+        'post': post,
+        'form': form
+    }
+    return render(request, 'form.html', context)
+
+
+
