46874d99381d2eccb8b090cfa5f9e57f58c9af51
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-16 16:06:10 +0530

    Add post detail view

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_list),
+    path('<slug:slug>/', views.post_detail, name="post-detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+
 
 def posts_list(request):
     objects = Post.objects.all()
@@ -9,3 +10,12 @@ def posts_list(request):
         'posts': objects
     }
     return render(request, 'list.html', context)
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    context = {
+        'post': post
+    }
+    return render(request, 'detail.html', context)
