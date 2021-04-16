1a711a658442d6e0ee71f5a5e28f7a9649c6b91d
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-16 15:54:11 +0530

    Initial view for posts list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def posts_list(request):
+    objects = Post.objects.all()
+
+    context = {
+        'posts': objects
+    }
+    return render(request, 'list.html', context)
